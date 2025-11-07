2b43bd9fb986c5980e9358a9d998db0aa02a17a9
Bryan Westcott <101424913+bryan-westcott@users.noreply.github.com>
2025-11-07 14:53:11 -0600

    Fix issues with SD migration to using shared _load_detector.  Ensure SD nano uses float16 as default dtype, as bnb 8-bit does not support bfloat16.

--- src/opguard/sd.py
+++ src/opguard/sd.py
@@ -4,7 +4,6 @@ import torch
 from diffusers import BitsAndBytesConfig as DiffusersBitsAndBytesConfig
 from diffusers import (
     DDPMScheduler,
-    DiffusionPipeline,
     StableDiffusionPipeline,
     StableDiffusionXLPipeline,
     UNet2DConditionModel,
@@ -23,6 +22,9 @@ from .vae import VaeSdxlFp16Fix, VaeTinyForSd
 
 class StableDiffusionBase(OpGuardBase):
     """Abstract class for Stable Diffusion (variants including 2.1 and XL)."""
+
+    SKIP_TO_DEVICE = True  # we are using device_map
+    SKIP_TO_DTYPE = True  # we are using device_map, so must be torch_dtype argument
 
     def _predict(
         self,
@@ -48,21 +50,6 @@ class StableDiffusionBase(OpGuardBase):
         output_raw = self._predict(prompt=input_raw, **kwargs)
         return self._postprocess(output_raw=output_raw)  # outupt_proc
 
-    def _load_detector(self) -> StableDiffusionXLPipeline | StableDiffusionPipeline:
-        pipe = DiffusionPipeline.from_pretrained(
-            self.model_id,
-            revision=self.REVISION,
-            variant=self.variant,
-            torch_dtype=self.dtype,
-            device_map=self.device_map,
-            use_safetensors=True,
-        )
-        pipe.enable_xformers_memory_efficient_attention()
-        pipe.enable_attention_slicing()
-        if self.device_map is None:
-            pipe = pipe.to(self.device)
-        return pipe
-
 
 class SdTinyNanoTextToImage(StableDiffusionBase):
     """A 4-bit quantized SD 2.1 Nano that uses around 2 GiB of VRAM.
@@ -74,6 +61,7 @@ class SdTinyNanoTextToImage(StableDiffusionBase):
     MODEL_ID = "bguisard/stable-diffusion-nano-2-1"
     REVISION = "main"
     DETECTOR_TYPE = StableDiffusionPipeline
+    DEFAULT_DTYPE = torch.float16  # bnb 4bit doesn't support bfloat16 as a starting point
 
     def _load_detector(self) -> object:
         # Swap in TinyVAE for SD1.x
@@ -105,9 +93,7 @@ class SdTinyNanoTextToImage(StableDiffusionBase):
             subfolder="scheduler",
         )
         # Load SD base pipeline
-        pipe = self.DETECTOR_TYPE.from_pretrained(
-            self.model_id,
-            revision=self.REVISION,
+        pipe: StableDiffusionPipeline = super()._load_detector(
             unet=unet,
             text_encoder=text_encoder,
             tokenizer=tokenizer,
@@ -128,9 +114,10 @@ class SdTinyNanoTextToImage(StableDiffusionBase):
         # Memory and performance tweaks
         pipe.enable_xformers_memory_efficient_attention()
         pipe.enable_attention_slicing()
+        if self.device_map is None:
+            pipe = pipe.to(device=self.device, dtype=self.dtype)
 
-        # Final device/dtype placement, with to() applied after VRAM savers
-        return pipe.to(self.device)
+        return pipe
 
 
 class SdxlTextToImage(StableDiffusionBase):
@@ -146,14 +133,7 @@ class SdxlTextToImage(StableDiffusionBase):
 
     def _load_detector(self) -> object:
         # Note: OpGuard will still free references on exit
-        pipe = self.DETECTOR_TYPE.from_pretrained(
-            self.model_id,
-            revision=self.REVISION,
-            variant=self.variant,
-            torch_dtype=self.dtype,
-            device_map=self.device_map,
-            use_safetensors=True,
-        )
+        pipe: StableDiffusionXLPipeline = super()._load_detector()
         with VaeSdxlFp16Fix(
             device_override=self.device,
             dtype_override=self.dtype,
