87d779da31684518d60b6099183f79880c25635c
Bryan Westcott <101424913+bryan-westcott@users.noreply.github.com>
2025-11-07 13:21:55 -0600

    Revert to actual kwarg name but also silence ruff and mypy

--- src/opguard/tests/conftest.py
+++ src/opguard/tests/conftest.py
@@ -6,13 +6,16 @@ from _pytest.config import Config
 from loguru import logger
 
 
-def pytest_configure(_config: Config) -> None:
+def pytest_configure(config: Config) -> None:
     """Set default logging level for pytest.
 
     Note: this will not have an effect unless pytest `-s` option is used,
     for example:
         >>> pytest -s -m smoke
     """
+    # mark as intentionally unused (silences Ruff ARG001/ARG002 and mypy)
+    _ = config
+
     logger.remove()
     # Note: set this to TRACE for maximum detail
     logger.add(sys.stderr, level="INFO")
