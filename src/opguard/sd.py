5d6aaac4da2fe397bab5ab44b6dc913c65ca6965
Bryan Westcott <101424913+bryan-westcott@users.noreply.github.com>
2025-11-10 17:19:32 -0600

    Added quant guard usage (and preferred load_pretrained) to sd models.

--- src/opguard/sd.py
+++ src/opguard/sd.py
@@ -1,7 +1,8 @@
 """Examples of Stable Diffusion."""
 
+from typing import Any, TypeAlias
+
 import torch
-from diffusers import BitsAndBytesConfig as DiffusersBitsAndBytesConfig
 from diffusers import (
     DDPMScheduler,
     StableDiffusionPipeline,
@@ -16,6 +17,9 @@ from transformers import CLIPTextModel, CLIPTokenizer
 from .base import OpGuardBase
 from .vae import VaeSdxlFp16Fix, VaeTinyForSd
 
+StableDiffusionPipelineLike: TypeAlias = StableDiffusionPipeline | StableDiffusionXLPipeline
+StableDiffusionPipelineOutputLike: TypeAlias = StableDiffusionPipelineOutput | StableDiffusionXLPipelineOutput
+
 # We do not care about LSP substitutability, OpGuard is not used directly
 # mypy: disable-error-code=override
 
@@ -23,20 +27,17 @@ from .vae import VaeSdxlFp16Fix, VaeTinyForSd
 class StableDiffusionBase(OpGuardBase):
     """Abstract class for Stable Diffusion (variants including 2.1 and XL)."""
 
-    SKIP_TO_DEVICE = True  # we are using device_map
-    SKIP_TO_DTYPE = True  # we are using device_map, so must be torch_dtype argument
-
     def _predict(
         self,
         *,
         prompt: str,
         **kwargs: object,
-    ) -> StableDiffusionXLPipelineOutput | StableDiffusionPipelineOutput:
+    ) -> StableDiffusionPipelineOutputLike:
         return self._detector(prompt=prompt, **kwargs)
 
     def _postprocess(
         self,
-        output_raw: StableDiffusionXLPipelineOutput | StableDiffusionPipelineOutput,
+        output_raw: StableDiffusionPipelineOutputLike,
     ) -> PILImage:
         # Simple postproc on the raw output, but return the config also
         return output_raw.images[0]
@@ -50,6 +51,10 @@ class StableDiffusionBase(OpGuardBase):
         output_raw = self._predict(prompt=input_raw, **kwargs)
         return self._postprocess(output_raw=output_raw)  # outupt_proc
 
+    def _load_detector(self, **kwargs: dict[str, Any]) -> StableDiffusionPipelineLike:
+        pipe: StableDiffusionPipelineLike = super()._load_detector(**kwargs)
+        return pipe
+
 
 class SdTinyNanoTextToImage(StableDiffusionBase):
     """A 4-bit quantized SD 2.1 Nano that uses around 2 GiB of VRAM.
@@ -61,27 +66,24 @@ class SdTinyNanoTextToImage(StableDiffusionBase):
     MODEL_ID = "bguisard/stable-diffusion-nano-2-1"
     REVISION = "main"
     DETECTOR_TYPE = StableDiffusionPipeline
-    DTYPE_PREFERENCE = torch.float16  # bnb 4bit doesn't support bfloat16 as a starting point
+    DEFAULT_DEVICE_MAP = "cuda"
+    DEFAULT_QUANT_TYPE = "nf4"
+    DEFAULT_QUANT_USE_DOUBLE = True
+    SKIP_TO_DEVICE = True
+    SKIP_TO_DTYPE = True
+    FROM_PRETRAINED_SKIP_KWARGS = ("quantization_config",)  # use individual components
 
     def _load_detector(self) -> object:
-        # Swap in TinyVAE for SD1.x
-        quant_config = DiffusersBitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,  # Enables double quantization
-            bnb_4bit_compute_dtype=torch.float16,
-        )
-
         unet = UNet2DConditionModel.from_pretrained(
             self.model_id,
             subfolder="unet",
-            quantization_config=quant_config,
+            quantization_config=self.quant_config,
             device_map="balanced",
         )
         text_encoder = CLIPTextModel.from_pretrained(
             self.model_id,
             subfolder="text_encoder",
-            quantization_config=quant_config,
+            quantization_config=self.quant_config,
             device_map="balanced",
         )
         tokenizer = CLIPTokenizer.from_pretrained(
@@ -93,7 +95,9 @@ class SdTinyNanoTextToImage(StableDiffusionBase):
             subfolder="scheduler",
         )
         # Load SD base pipeline
-        pipe: StableDiffusionPipeline = super()._load_detector(
+        # Note: individual components must be quantized,
+        #       as it doesn't accept quantization_config argument
+        pipe = super()._load_detector(
             unet=unet,
             text_encoder=text_encoder,
             tokenizer=tokenizer,
@@ -110,13 +114,6 @@ class SdTinyNanoTextToImage(StableDiffusionBase):
             device_map_override=self.device_map,
         ) as vae:
             pipe.vae = vae.detector
-
-        # Memory and performance tweaks
-        pipe.enable_xformers_memory_efficient_attention()
-        pipe.enable_attention_slicing()
-        if self.device_map is None:
-            pipe = pipe.to(device=self.device, dtype=self.dtype)
-
         return pipe
 
 
@@ -130,18 +127,16 @@ class SdxlTextToImage(StableDiffusionBase):
     DEFAULT_DTYPE = torch.bfloat16
     DEFAULT_DEVICE_MAP = "cuda"
     DETECTOR_TYPE = StableDiffusionXLPipeline
+    SKIP_TO_DEVICE = True
+    SKIP_TO_DTYPE = True
 
     def _load_detector(self) -> object:
         # Note: OpGuard will still free references on exit
-        pipe: StableDiffusionXLPipeline = super()._load_detector()
+        pipe = super()._load_detector()
         with VaeSdxlFp16Fix(
             device_override=self.device,
             dtype_override=self.dtype,
             device_map_override=self.device_map,
         ) as vae:
             pipe.vae = vae.detector
-        pipe.enable_xformers_memory_efficient_attention()
-        pipe.enable_attention_slicing()
-        if self.device_map is None:
-            pipe = pipe.to(self.device)
         return pipe
